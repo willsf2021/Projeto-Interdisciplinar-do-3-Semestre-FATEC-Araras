6a15c3e353de67028433ed8334c34e75c7d6f974
willsf2021 <willsf2021@gmail.com>
2025-10-05 22:36:12 -0300

    feat: transition added and some goods

--- frontend/src/Views/Login/index.jsx
+++ frontend/src/Views/Login/index.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
 import "./styles.css";
+
 export const Login = () => {
-  return <h1>Login page</h1>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  return (
+    <div className={`login_container ${isVisible ? "visible" : ""}`}>
+      <h1>Página de Login</h1>
+    </div>
+  );
 };

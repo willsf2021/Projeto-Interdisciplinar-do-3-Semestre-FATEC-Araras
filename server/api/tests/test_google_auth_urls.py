35cdfd1fb8360997b7b87250d68bcd8818111f62
Wilson Junior <wilson.junior@mxdrive.com.br>
2025-10-27 14:32:55 -0300

    testes: teste de url do google auth

---
+++ server/api/tests/test_google_auth_urls.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class GoogleAuthUrlsTest(TestCase):
+    def test_google_auth_url_reverse(self):
+        url = reverse('google_auth')
+        self.assertEqual(url, '/api/google-auth/') 
+
+

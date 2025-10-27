a6bdffe58d79ae52babe66f8d2840bb2af9111a0
Wilson Junior <wilson.junior@mxdrive.com.br>
2025-10-27 14:36:04 -0300

    testes: corrigido testes de receitas

--- server/api/tests/test_receita_model.py
+++ server/api/tests/test_receita_model.py
@@ -9,9 +9,10 @@ class ReceitaModelTest(TestCase):
     def setUp(self):
         """Configuração inicial para cada teste"""
         self.usuario = Usuario.objects.create_user(
+            name="Teste da Silva",
             email='teste@example.com',
             password='senha123',
-            tipo='profissional'
+            type='profissional'
         )
         
         self.receita = Receita.objects.create(

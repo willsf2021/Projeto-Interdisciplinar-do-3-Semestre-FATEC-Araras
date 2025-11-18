8f2f95abdf44baed150033ee1460677971f25512
Wilson Junior <wilson.junior@mxdrive.com.br>
2025-11-18 16:54:08 -0300

    Updates

---
+++ sw.js
@@ -0,0 +1,29 @@
+// public/sw.js
+const CACHE_NAME = 'rotus-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
